6b5034946f899ae80a176d21809a40afd31fc7ea
Honoka <rakuraku2563nonnon@gmail.com>
2020-04-03 11:02:56 +0900

    test

--- mamazon/views.py
+++ mamazon/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,LiistView
+from .models import Product
 
 
 class Home(TemplateView):
     template_name = 'mamazon/home.html'
+
+class ProductListView(ListView):
+    model = Product
+    template_name='mamazon/list.html'
