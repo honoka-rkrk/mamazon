ab827bb7cddf70b3d94fef6551f3f05ded6f371c
Honoka <rakuraku2563nonnon@gmail.com>
2020-04-03 12:36:43 +0900

    lesson07

---
+++ cart/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from dajango.conf import settings
+from mamazon.models import Product
+
+User=settings.AUTH_USER_MODEL
+
+class Cart(models.Model):
+    user=models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
+    products=models.ManyToManyField(Product,blank=True)
+    total=models.DecimalField(default=0.00,max_disits=9,decimal_places=2) 
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
